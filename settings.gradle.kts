984df45162ef005c036bb4bf6cec04d5d94804ea
DolphaGo <adamdoha@naver.com>
2023-01-18 02:00:17 +0900

    feat: Init projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "board"
 
 include("board-entity")
 include("board-api")
+include("board-support")
 include("board-front:board-front-api")
 findProject(":board-front:board-front-api")?.name = "board-front-api"
 
